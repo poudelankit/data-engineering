42217342752af681b1a77c7bb7b8d135adaeafa0
ankitpoudel <poudelankit101@gmail.com>
2026-07-28 08:52:19 +0545

    testing comment added

--- azure-function-app/function_app.py
+++ azure-function-app/function_app.py
@@ -8,3 +8,5 @@ app = func.FunctionApp()
 @app.event_grid_trigger(arg_name="azeventgrid")
 def EventGridTrigger(azeventgrid: func.EventGridEvent):
     logging.info('Python EventGrid trigger processed an event')
+
+    #commented to check the working of cicd pipeline
